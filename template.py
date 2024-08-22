79fce55ea9751e321ad8b4a637cf567c82aed541
Syed Noor Mohammed <syednoor.m@techsophy.com>
2024-08-22 11:29:18 +0530

    Init

---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/utils/__init__.py",
+    "src/utils/common.py",
+    "src/logging/__init__.py",
+    "src/config/__init__.py",
+    "src/config/configuration.py",
+    "src/pipeline/__init__.py",
+    "src/entity/__init__.py",
+    "src/constants/__init__.py",
+    "tests/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "requirements-dev.txt",
+    ".gitignore",
+    "setup.py",
+    "research/trials.ipynb",
+    "setup.cfg",
+    'artifacts/.gitkeep',
+    "myproject.toml",
+    "tox.ini"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
