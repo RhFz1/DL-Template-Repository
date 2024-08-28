ec296a529de08a835d8c8744360b2be893f9a4c9
RhFz1 <nooralifirst@gmail.com>
2024-08-28 16:49:01 +0530

    Add trainconfig params

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from dotenv import load_dotenv
 from dataclasses import dataclass
 from typing import Tuple, List
@@ -16,3 +17,14 @@ class DataTransformationConfig():
     reshape: Tuple[int, int] = (28, 28)
     std: List[int, int, int] = [0.229, 0.224, 0.225]
     mean: List[int, int, int] = [0.485, 0.456, 0.406]
+    file_type: str = os.getenv('DATA_FILE_TYPE')
+
+@dataclass
+class TrainingConfig():
+    epochs: int = 10
+    batch_size: int = 64
+    learning_rate: float = 0.001
+    device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+    weight_decay: float = 0.0001
+    momentum: float = 0.9
+    betas: Tuple[float, float] = (0.9, 0.999)
