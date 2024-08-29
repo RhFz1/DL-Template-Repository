18e88e2014398b046dff523a258c593ccaab92e2
RhFz1 <nooralifirst@gmail.com>
2024-08-29 16:24:11 +0530

    Include model saving

--- src/pipeline/train.py
+++ src/pipeline/train.py
@@ -6,7 +6,14 @@ from torch.utils.tensorboard import SummaryWriter
 from src.constants.model import Model
 from src.constants.dataloader import DataLoader
 from src.config.config import TrainingConfig
+from src.utils.common import save_checkpoint, load_checkpoint
 import uuid
+import argparse
+
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--resume_training', type=bool, default=False)
+args = arg_parser.parse_args()
 
 # Load training configuration
 training_config = TrainingConfig()
@@ -22,13 +29,18 @@ betas = training_config.betas
 
 # Initialize the model and move it to the specified device
 model = Model()
-model.to(device)
 
 # Set up the optimizer, loss function, and TensorBoard writer
 optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=momentum, betas=betas)
 criterion = nn.CrossEntropyLoss(reduction='mean')
 writer = SummaryWriter(f'./artifacts/runs/experiment_{uuid.uuid4()}')
 
+best_val_loss = 100.00
+if args.resume_training:
+    model, optimizer, best_val_loss = load_checkpoint(model, optimizer, model_dir=training_config.registry)
+
+model = model.to(device)
+model = model.compile()
 # Load the data
 data_loader = DataLoader()
 train_loader, val_loader = data_loader.load(batch_size=batch_size)
@@ -111,6 +123,11 @@ class Trainer():
             # Log validation metrics
             writer.add_scalar('validation_loss', val_loss, epoch)
             writer.add_scalar('validation_accuracy', val_accuracy, epoch)
+
+            # Save the best model
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                save_checkpoint(self.model, self.optimizer, training_config.registry)
 
 
 if __name__ == '__main__':
